fe8a886a7acb2a1ce002d1090b05187c244daf80
rite2harini <rite2harini@gmail.com>
2026-06-06 14:00:28 +0530

    Create Online_Exam_System.java

---
+++ Online_Exam_System.java
@@ -0,0 +1,12 @@
+/*
+Create a program that:
+
+Takes student answers score
+Calculates percentage
+Rules:
+Marks cannot exceed 100 → InvalidMarksException
+Negative marks not allowed → NegativeMarksException
+If student ID is null → NullPointerException handling
+Twist:
+Use nested try-catch blocks
+*/
